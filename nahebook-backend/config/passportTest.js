c87995f913467c1d9910827572805e76401322fb
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-28 15:31:16 +0200

    Feat: define logic authentication + authorization
    * To allow users to signin passport local strategy is used for
    authentication
    * JSON Web Tokens are used to protect routes to authorized users
    * passport file is for production and passportTest is for development
    using a mock DB

---
+++ nahebook-backend/config/passportTest.js
@@ -0,0 +1,44 @@
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const LocalStrategy = require("passport-local");
+require("./databaseTest");
+const JWTstrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+require("dotenv").config();
+const opts = {};
+opts.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.secret;
+const User = require("../models/user");
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+
+passport.use(
+  new JWTstrategy(opts, async (jwt_payload, done) => {
+    try {
+      const user = await User.findById(jwt_payload.user._id);
+      if (!user) {
+        return done(null, false);
+      } else {
+        return done(null, user);
+      }
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
