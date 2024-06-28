15351e4bdb2bc1e3176b2e8a97e3d7b2445dcc7a
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-28 16:41:41 +0200

    Feat: route + define logic to get&update profile

--- nahebook-backend/controllers/UserController.js
+++ nahebook-backend/controllers/UserController.js
@@ -96,3 +96,81 @@ exports.signin = [
     })(req, res, next);
   }),
 ];
+
+exports.get_profile = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(
+    req.params.userId,
+    "first_name last_name username age bio profile_picture posts following",
+  ).exec();
+
+  if (!user) {
+    const err = { message: "User not found", status: 404 };
+    return next(err);
+  } else {
+    return res.status(200).json({ user });
+  }
+});
+
+exports.update_profile = [
+  body("first_name", "First name must be between 2 and 50 characters")
+    .trim()
+    .isLength({ min: 2, max: 50 })
+    .escape(),
+  body("last_name", "Last name must be between 2 and 50 characters")
+    .trim()
+    .isLength({ min: 2, max: 50 })
+    .escape(),
+  body("username", "Username must be between 5 and 20 characters")
+    .trim()
+    .isLength({ min: 5, max: 20 })
+    .escape(),
+  body("password", "Password must be between 5 and 80 characters")
+    .trim()
+    .isLength({ min: 5, max: 100 })
+    .escape(),
+  body("age", "Age must be a number").trim().isInt().optional().escape(),
+  body("bio", "Bio must be between one and 200 characters")
+    .trim()
+    .isLength({ min: 1, max: 200 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const currentUser = await User.findById(req.params.userId).exec();
+
+    try {
+      bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+        const user = new User({
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          username: req.body.username,
+          password: hashedPassword,
+          age: req.body.age,
+          bio: req.body.bio,
+          profile_picture: currentUser.profile_picture,
+          posts: currentUser.posts,
+          following: currentUser.following,
+          _id: currentUser._id,
+        });
+
+        if (
+          currentUser.username !== req.body.username &&
+          (await User.exists({ username: req.body.username }))
+        ) {
+          return res
+            .status(500)
+            .json({ message: "Username already exists", user });
+        } else if (!errors.isEmpty()) {
+          return res.status(500).json({ errors, user });
+        } else if (err) {
+          throw err;
+        } else {
+          await User.findByIdAndUpdate(currentUser._id, user).exec();
+          return res.status(200).json({ message: "Your profile is updated" });
+        }
+      });
+    } catch (err) {
+      return next(err);
+    }
+  }),
+];
--- nahebook-backend/routes/users.js
+++ nahebook-backend/routes/users.js
@@ -1,10 +1,23 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
 
 const user_controller = require("../controllers/UserController");
 
 router.post("/signin", user_controller.signin);
 
 router.post("/signup", user_controller.signup);
+
+router.get(
+  "/profile/:userId",
+  passport.authenticate("jwt", { session: false }),
+  user_controller.get_profile,
+);
+
+router.put(
+  "/profile/:userId",
+  passport.authenticate("jwt", { session: false }),
+  user_controller.update_profile,
+);
 
 module.exports = router;
