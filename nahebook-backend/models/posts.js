280f6f6198652272061e814a39f42e373c74e3bd
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-06-30 18:53:45 +0200

    Refactor: change date type to string
    * In order to use custom dates with date-fns a string type is needed

--- nahebook-backend/models/posts.js
+++ nahebook-backend/models/posts.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const PostsSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  date: { type: Date, required: true },
+  date: { type: String, required: true },
   text: { type: String, required: false, maxLength: 50 },
   imgUrl: { type: String, required: false },
   likes: [{ type: Schema.Types.ObjectId, required: false, ref: "User" }],
