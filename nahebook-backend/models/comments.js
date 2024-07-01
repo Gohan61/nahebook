2fd049b25977977e31232a03bf3cfaeb3fe002d5
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-07-01 08:21:35 +0200

    Refactor: use lowercase d on postId & userId
    * As Id is spelled with lowercase d throughout the application, using
    different spelling here is asking for bugs

--- nahebook-backend/models/comments.js
+++ nahebook-backend/models/comments.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CommentsSchema = new Schema({
-  userID: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
   username: { type: Schema.Types.String, ref: "User", required: true },
   timestamp: { type: String, required: true },
   text: { type: String, required: true, minLength: 1, maxLength: 30 },
-  postID: { type: Schema.Types.ObjectId, ref: "Posts", required: true },
+  postId: { type: Schema.Types.ObjectId, ref: "Posts", required: true },
 });
 
 module.exports = mongoose.model("Comments", CommentsSchema);
