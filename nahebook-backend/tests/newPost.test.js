5a3e7f90ddcc87cf1385a857c16dc9a5ee404830
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-07-01 09:01:16 +0200

    Test: delete a post + errors

--- nahebook-backend/tests/newPost.test.js
+++ nahebook-backend/tests/newPost.test.js
@@ -6,6 +6,7 @@ const post = require("../routes/post");
 const initializeMongoServer = require("../config/databaseTest");
 require("../config/passportTest");
 const Usermodel = require("../models/user");
+const Postmodel = require("../models/posts");
 const bcrypt = require("bcryptjs");
 let token;
 const userId = "621ff30d2a3e781873fcb663";
@@ -33,6 +34,16 @@ beforeAll(async () => {
   } catch (err) {
     console.log(err);
   }
+
+  const post = new Postmodel({
+    text: "Hello world",
+    imgUrl: "http://test.io",
+    date: "01-01-2022",
+    userId: userId,
+    _id: "621ff30d2a3e781873fcb669",
+  });
+
+  await post.save();
 });
 
 app.use(express.json({ limit: "10mb" }));
@@ -90,7 +101,7 @@ test("Returns authorization error", async () => {
     });
 });
 
-test("User can create a new post", async () => {
+test("Throws validation error", async () => {
   const payload = {
     text: "Hello world Hello worldHello worldHello worldHello worldHello world",
     userId: userId,
@@ -110,5 +121,34 @@ test("User can create a new post", async () => {
       expect(res.body.errors.errors[0].msg).toMatch(
         /Maximun post description is 50 characters/,
       );
+    });
+});
+
+test("User can delete a post", async () => {
+  const authorization = {
+    Authorization: `Bearer ${token}`,
+  };
+
+  const res = await request(app)
+    .delete("/post/621ff30d2a3e781873fcb669")
+    .set(authorization)
+    .set("Content-Type", "application/json")
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body.message).not.toBeNull();
+    });
+});
+
+test("Throws 404 error for non existing post", async () => {
+  const authorization = {
+    Authorization: `Bearer ${token}`,
+  };
+
+  const res = await request(app)
+    .delete("/post/621ff30d2a3e781873fcb610")
+    .set(authorization)
+    .set("Content-Type", "application/json")
+    .then((res) => {
+      expect(res.status).toBe(404);
     });
 });
