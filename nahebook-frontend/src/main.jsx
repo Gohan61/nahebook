c0853517e9a82b7cf08d882928a74521c21f625e
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-07-02 16:05:29 +0200

    Feat: define routes to Signin + Signup pages

--- nahebook-frontend/src/main.jsx
+++ nahebook-frontend/src/main.jsx
@@ -4,12 +4,18 @@ import App from "./components/App";
 import "./stylesheets/index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Homepage from "./components/Homepage";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ index: true, element: <Homepage /> }],
+    children: [
+      { index: true, element: <Homepage /> },
+      { path: "signin", element: <Signin /> },
+      { path: "signup", element: <Signup /> },
+    ],
   },
 ]);
 
