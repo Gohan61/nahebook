04fb3f3d2171847f4acba289793b3c265573eaaa
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-07-02 17:28:49 +0200

    Test: render/hide validation errors on login

---
+++ nahebook-frontend/src/tests/Signin.test.jsx
@@ -0,0 +1,82 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import Signin from "../components/Signin";
+import Signup from "../components/Signup";
+import userEvent from "@testing-library/user-event";
+
+vi.mock("react-router-dom", () => {
+  const originalModule = vi.importActual("react-router-dom");
+  const loginStatus = false;
+  const setLoginStatus = () => {};
+
+  return {
+    _esModule: true,
+    ...originalModule,
+    useOutletContext: () => [loginStatus, setLoginStatus],
+    useNavigate: vi.fn(),
+  };
+});
+
+describe("Signup", () => {
+  it("Should sign user up", async () => {
+    const user = userEvent.setup();
+
+    render(<Signup />);
+
+    const first_nameInput = screen.getByLabelText("First name:");
+    const last_nameInput = screen.getByLabelText("Last name:");
+    const usernameInput = screen.getByLabelText("Username:");
+    const passwordInput = screen.getByLabelText("Password");
+    const ageInput = screen.getByLabelText("Age:");
+    const bioInput = screen.getByLabelText("Bio:");
+    const submitButton = screen.getByText("Submit");
+
+    await userEvent.type(first_nameInput, "testing");
+    await userEvent.type(last_nameInput, "testing");
+    await userEvent.type(usernameInput, "testing");
+    await userEvent.type(passwordInput, "testing");
+    await userEvent.type(ageInput, "12");
+    await userEvent.type(bioInput, "testing testing testing");
+
+    await user.click(submitButton);
+
+    expect(await screen.findByTestId("signupErrors")).toBeEmpty();
+  });
+});
+
+describe("Signin", () => {
+  it("Should display validation errors", async () => {
+    const user = userEvent.setup();
+
+    render(<Signin />);
+
+    const usernameInput = screen.getByLabelText("Username:");
+    const passwordInput = screen.getByLabelText("Password:");
+    const submitButton = screen.getByText("Submit");
+
+    await userEvent.type(usernameInput, " ");
+    await userEvent.type(passwordInput, " ");
+
+    await user.click(submitButton);
+
+    expect(await screen.findByText("Username must be at least 5 characters"));
+    expect(await screen.findByText("Password must be at least 5 characters"));
+  });
+
+  it("Should log user in", async () => {
+    const user = userEvent.setup();
+
+    render(<Signin />);
+
+    const usernameInput = screen.getByLabelText("Username:");
+    const passwordInput = screen.getByLabelText("Password:");
+    const submitButton = screen.getByText("Submit");
+
+    await userEvent.type(usernameInput, "testing");
+    await userEvent.type(passwordInput, "testing");
+
+    await user.click(submitButton);
+
+    expect(await screen.findByTestId("signinErrors")).toBeEmptyDOMElement();
+  });
+});
