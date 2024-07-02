10ecf43f66ef463b62d2213b25ff0898dd08fc1f
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-07-02 07:24:40 +0200

    Feat: route Homepage as index

--- nahebook-frontend/src/main.jsx
+++ nahebook-frontend/src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./stylesheets/index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Homepage from "./components/Homepage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [],
+    children: [{ index: true, element: <Homepage /> }],
   },
 ]);
 
