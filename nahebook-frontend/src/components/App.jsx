c479784ad3710a61f0106ff0e7f3c729ccb004cf
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-07-02 16:14:30 +0200

    Feat: define loginStatus state + conditonal links
    * Depending upon loginStatus, logout or signup/signin links are shown

--- nahebook-frontend/src/components/App.jsx
+++ nahebook-frontend/src/components/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import "../stylesheets/App.css";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
 
 function App() {
+  const [loginStatus, setLoginStatus] = useState(() => {
+    if (localStorage.getItem("Token")) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <Navbar props={{ loginStatus, setLoginStatus }} />
+      <Outlet context={{ loginStatus, setLoginStatus }} />
     </>
   );
 }
--- nahebook-frontend/src/components/Navbar.jsx
+++ nahebook-frontend/src/components/Navbar.jsx
@@ -1,8 +1,34 @@
-export default function Navbar() {
+import { Link, useNavigate } from "react-router-dom";
+
+export default function Navbar({ props }) {
+  const navigate = useNavigate();
+
   return (
     <nav>
       <a href="/">Nahebook</a>
-      <div className="links"></div>
+      <div className="links">
+        {props.loginStatus ? (
+          <>
+            <button
+              onClick={() => {
+                localStorage.removeItem("Token");
+                localStorage.removeItem("userId");
+                localStorage.removeItem("username");
+                props.setLoginStatus(false);
+                navigate("/");
+              }}
+              className="logoutButton"
+            >
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to={"signin"}>Sign in</Link>
+            <Link to={"signup"}>Sign up</Link>
+          </>
+        )}
+      </div>
     </nav>
   );
 }
