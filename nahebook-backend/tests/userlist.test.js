6093add644f1b82c63fea2676c4b4adebb6c2916
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-07-04 08:04:33 +0200

    Test: separate user lists are returned

--- nahebook-backend/tests/userlist.test.js
+++ nahebook-backend/tests/userlist.test.js
@@ -8,6 +8,7 @@ const Usermodel = require("../models/user");
 const bcrypt = require("bcryptjs");
 let token;
 const userId = "621ff30d2a3e781873fcb663";
+const followerId = "621ff30d2a3e781873fcb664";
 
 beforeAll(async () => {
   await initializeMongoServer();
@@ -25,7 +26,7 @@ beforeAll(async () => {
         _id: "621ff30d2a3e781873fcb663",
         profile_picture: "http://test.io",
         posts: ["621ff30d2a3e781873fcb669"],
-        following: "621ff30d2a3e781873fcb664",
+        following: ["621ff30d2a3e781873fcb664"],
       });
       await user1.save();
     });
@@ -39,6 +40,7 @@ beforeAll(async () => {
         age: 14,
         bio: "I am new here testing things out for second user",
         _id: "621ff30d2a3e781873fcb664",
+        following: ["621ff30d2a3e781873fcb663"],
       });
 
       await user2.save();
@@ -77,21 +79,76 @@ test("Returns list of users", async () => {
   };
 
   const res = await request(app)
-    .get("/user/list")
+    .get(`/user/list/${userId}`)
     .set("Content-Type", "application/json")
     .set(authorization)
     .then((res) => {
       expect(res.status).toBe(200);
-      expect(res.body.users.length).toBe(2);
+      expect(res.body.user.following.length).toBe(1);
+      expect(res.body.users.length).toBe(0);
     });
 });
 
 test("Denies access to route with 401 code", async () => {
   const res = await request(app)
-    .get("/user/list")
+    .get(`/user/list/${userId}`)
     .set("Content-Type", "application/json")
     .then((res) => {
-      console.log(res);
       expect(res.status).toBe(401);
+    });
+});
+
+test("User can send follow request", async () => {
+  const authorization = {
+    Authorization: `Bearer ${token}`,
+  };
+
+  const payload = {
+    followUserId: followerId,
+  };
+
+  let user;
+  let followedUser;
+
+  const res = await request(app)
+    .post(`/user/follow/${userId}`)
+    .set(authorization)
+    .set("Content-Type", "application/json")
+    .send(payload)
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body.message).not.toBeFalsy();
+    })
+    .then(async () => {
+      user = await Usermodel.findById(
+        userId,
+        "pendingFollow receivedRequestFollow",
+      ).exec();
+      followedUser = await Usermodel.findById(
+        followerId,
+        "pendingFollow receivedRequestFollow",
+      ).exec();
+
+      expect(user.pendingFollow.length).toBe(1);
+      expect(user.receivedRequestFollow.length).toBe(0);
+      expect(followedUser.receivedRequestFollow.length).toBe(1);
+      expect(followedUser.pendingFollow.length).toBe(1);
+    });
+});
+
+test("Returns pendingFollow user in user object and not in users", async () => {
+  const authorization = {
+    Authorization: `Bearer ${token}`,
+  };
+
+  const res = await request(app)
+    .get(`/user/list/${userId}`)
+    .set("Content-Type", "application/json")
+    .set(authorization)
+    .then((res) => {
+      console.log(res.body);
+      expect(res.status).toBe(200);
+      expect(res.body.users.length).toBe(0);
+      expect(res.body.user.following.length).toBe(1);
     });
 });
