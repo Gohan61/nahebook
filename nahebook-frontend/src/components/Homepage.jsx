89e1329e6592844fe261bb2e0ad03ab5c36a2bc1
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-07-08 16:48:20 +0200

    Stye: display images on Homepage + align
    * Images will align/stack horizontally on smaller screens to prevent
    overflow

--- nahebook-frontend/src/components/Homepage.jsx
+++ nahebook-frontend/src/components/Homepage.jsx
@@ -1,7 +1,19 @@
+import cows from "../assets/Cows.jpg";
+import flowers from "../assets/Flowers.jpg";
+import shadow from "../assets/Shadow.jpg";
+import mountain from "../assets/Mountains.jpg";
+import "../stylesheets/Homepage.css";
+
 export default function Homepage() {
   return (
     <div className="homepage">
       <h1>Welcome to Nahebook</h1>
+      <div className="images">
+        <img src={cows} alt="" />
+        <img src={mountain} alt="" />
+        <img src={flowers} alt="" />
+        <img src={shadow} alt="" />
+      </div>
     </div>
   );
 }
